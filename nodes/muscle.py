6ec6ce6e0f9e5f670bf17a1922b1633778d9f124
Mikkel Schubert <MikkelSch@gmail.com>
2012-07-03 15:21:12 +0200

    Rework muscle and sequences nodes.

--- nodes/muscle.py
+++ nodes/muscle.py
@@ -1,23 +1,46 @@
-from pypeline.node import CommandNode
+import os
+
+from pypeline.node import CommandNode, MetaNode
 from pypeline.atomiccmd import AtomicCmd
 
 
 class MuscleNode(CommandNode):
-    def __init__(self, infile, outfile, dependencies = ()):
-        call = ["muscle", 
+    def __init__(self, infile, prefix, dependencies = ()):
+        call = ["muscle",
+                "-quiet",
                 "-maxiters", 32,
                 "-in", "%(IN_FASTA)s",
-                "-physout", "%(OUT_PHYS)s"]
+                "-fastaout", "%(OUT_AFA)s",
+                "-phyiout",  "%(OUT_PHYS)s"]
 
         command = AtomicCmd(call,
                             IN_FASTA = infile,
-                            OUT_PHYS = outfile)
+                            OUT_AFA  = prefix + ".afa",
+                            OUT_PHYS = prefix + ".phy")
 
         description = "<MuscleNode: '%s' -> '%s'>" \
-                % (infile, outfile)
+                % (infile, prefix + ".*")
 
         CommandNode.__init__(self,
                              command      = command,
                              description  = description,
                              dependencies = dependencies)
-                             
+ 
+
+
+
+class MetaMuscleNode(MetaNode):
+    def __init__(self, rootdir, sequences, dependencies = ()):
+        subnodes = []
+        for sequence in sequences:
+            prefix  = os.path.join(rootdir, sequence)
+            node    = MuscleNode(infile       = prefix + ".fasta",
+                                 prefix       = prefix, 
+                                 dependencies = dependencies)
+
+            subnodes.append(node)
+
+        MetaNode.__init__(self,
+                          description = "<MuscleAlignSequences: In '%s'>" \
+                                % (rootdir,),
+                          subnodes    = subnodes)                            
--- nodes/sequences.py
+++ nodes/sequences.py
@@ -9,31 +9,48 @@ from pypeline.node import Node
 
 
 class CollectSequencesNode(Node):
-    def __init__(self, infiles, outfile, sequence, dependencies):
-        self._infiles = dict(infiles)
-        self._outfile = outfile
-        self._sequence = sequence
+    def __init__(self, infiles, destination, sequences, dependencies):
+        self._infiles     = dict(infiles)
+        self._destination = str(destination)
+        self._sequences   = list(sequences)
+        self._outfiles    = []
+
+        for sequence in self._sequences:
+            self._outfiles.append(os.path.join(destination, sequence + ".fasta"))
 
         Node.__init__(self,
-                      description  = "<CollectSequences: '%s' from %i files -> '%s'>" \
-                            % (sequence, len(infiles), outfile),
+                      description  = "<CollectSequences: %i sequences from %i files -> '%s'>" \
+                            % (len(sequences), len(self._infiles), destination),
                       input_files  = self._infiles.values(),
-                      output_files = [self._outfile],
+                      output_files = self._outfiles,
                       dependencies = dependencies)
 
 
     def _run(self, _config, temp):
-        filename = os.path.join(temp, os.path.basename(self._outfile))
+        handles = []
+        for (name, source) in sorted(self._infiles.items()):
+            handles.append((name, pysam.Fastafile(source)))
 
-        with open(filename, "w") as fasta:
-            for (name, source) in self._infiles.iteritems():
-                handle = pysam.Fastafile(source)
-                sequence = textwrap.fill(handle.fetch(self._sequence), 60)
-                handle.close()
-                
-                fasta.write(">%s\n%s\n" % (name, sequence))
+        for sequence in self._sequences:
+            filename = os.path.join(temp, sequence + ".fasta")
+
+            with open(filename, "w") as fasta:
+                for (name, handle) in handles:
+                    fastaseq = textwrap.fill(handle.fetch(sequence), 60)
+                    assert fastaseq, (name, sequence)
+                    fasta.write(">%s\n%s\n" % (name, fastaseq))
+
+        for (name, handle) in handles:
+            handle.close()
 
 
     def _teardown(self, _config, temp):
-        filename = os.path.join(temp, os.path.basename(self._outfile))
-        os.rename(filename, self._outfile)
+        if not os.path.exists(self._destination):
+            os.makedirs(self._destination)
+
+        for sequence in self._sequences:
+            filename = sequence + ".fasta"
+            infile   = os.path.join(temp, filename)
+            outfile  = os.path.join(self._destination, filename)
+        
+            os.rename(infile, outfile)
