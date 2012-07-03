d50dd878e5c3c9ff1303e27a764fe0e4c8f71236
Mikkel Schubert <MikkelSch@gmail.com>
2012-07-03 20:26:17 +0200

    Adding initial version of Task graph class.

---
+++ taskgraph.py
@@ -0,0 +1,90 @@
+
+
+class TaskGraph:
+    DONE, RUNNING, RUNABLE, QUEUED, OUTDATED, ERROR = range(6)
+
+
+    def __init__(self, tasks):
+        self._tasks        = list(tasks)
+        self._states       = None
+        self._fixed_states = {}
+
+
+    def mark_task(self, task, state):
+        self._fixed_states[task] = state
+        self._states = None
+
+
+    def clear_task_state(self, task):
+        del self._fixed_states[task]
+        self._states = None
+
+
+    def get_state(self, task):
+        return self._get_states()[task]
+
+
+    def get_runable_tasks(self, max_tasks):
+        max_tasks = max(0, max_tasks - self.running_tasks())
+
+        for (task, state) in self._get_states().items():
+            if not max_tasks:
+                break
+            elif state == self.RUNABLE:
+                yield task
+                max_tasks -= 1
+
+
+    def any_runable_left(self):
+        for state in self._get_states().itervalues():
+            if state in (self.RUNNING, self.RUNABLE):
+                return True
+
+        return False
+
+    
+    def running_tasks(self):
+        return self._get_states().count(self.RUNNING)
+
+
+    def _get_states(self):
+        if self._states is None:
+            self._states = self._build_state_map(self._tasks, self._fixed_states)
+
+        return self._states
+
+
+    @classmethod
+    def _build_state_map(cls, tasks, fixed_states):
+        states = dict(fixed_states)
+        for task in tasks:
+            cls._update_state_map(task, states)
+
+        return states
+
+
+    @classmethod
+    def _update_state_map(cls, task, states):
+        if task in states:
+            return states[task]
+
+        state = cls.DONE
+        for subtask in task.subnodes:
+            state = max(state, cls._update_state_map(subtask, states))
+
+        if state == cls.DONE:
+            if not task.is_done:
+                state = cls.RUNABLE
+            elif task.is_outdated:
+                state = cls.OUTDATED
+
+            states[task] = state
+        elif state in (cls.RUNNING, cls.RUNABLE, cls.QUEUED):
+            if task.is_done:
+                states[task] = cls.OUTDATED
+            else:
+                states[task] = cls.QUEUED
+        else:
+            states[task] = state
+
+        return state
