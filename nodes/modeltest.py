8ca0befd993e278f5baa1530a528a005ac2c0507
Mikkel Schubert <MikkelSch@gmail.com>
2012-07-10 13:26:01 +0200

    Adding jModelTest node.

---
+++ nodes/modeltest.py
@@ -0,0 +1,32 @@
+import os
+
+from pypeline.node import CommandNode
+from pypeline.atomiccmd import AtomicCmd
+
+
+
+class JModelTestNode(CommandNode):
+    pileup_args = "-EA"
+
+    def __init__(self, config, infile, outfile, threads = 1, dependencies = ()):
+        call = ["java", "-Djava.awt.headless=true",
+                "-jar", os.path.join(config.jar_root, "jModelTest.jar"),
+                "-f",         # Include models with unequals base frecuencies 
+                "-i",         # Include models with a proportion invariable sites
+                "-g", 8,      # Include models with rate variation among sites and number of categories
+                "-s", 11,     # Number of substitution schemes
+                "-S", "BEST", # Tree topology search operation option
+                "-AICc",      # Calculate the corrected Akaike Information Criterion
+                "-BIC",       # Calculate the Bayesian Information Criterion
+                "-tr", threads,
+		"-d", "%(IN_ALIGNMENT)s"]
+
+        jmodeltest  = AtomicCmd(call,
+                                IN_ALIGNMENT = infile,
+                                stdout       = outfile)
+
+        description = "<jModelTest: '%s' -> '%s'>" % (infile, outfile)
+        CommandNode.__init__(self, 
+                             description  = description,
+                             command      = jmodeltest,
+                             dependencies = dependencies)
