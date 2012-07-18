8b13fdf8de40c4cbdc1f024002cc6e969a78acae
Mikkel Schubert <MikkelSch@gmail.com>
2012-07-18 23:06:19 +0200

    Adding (nose) unittests.

---
+++ tests/atomicset_test.py
@@ -0,0 +1,158 @@
+import nose
+
+from flexmock import flexmock
+
+from atomiccmd import AtomicCmd, CmdError
+from atomicset import ParallelCmds, SequentialCmds
+
+
+################################################################################
+################################################################################
+## Functions and properties with same expected behavior for each set
+
+_COMMON_RETURNS = [1, 2, 3]
+_COMMON_FUNCTIONS = [
+    ("join",         [],      _COMMON_RETURNS),
+    ("commit",       ["TMP"], None) ]
+_COMMON_PROPERTIES = [
+    ("executables",  _COMMON_RETURNS),
+    ("input_files",  _COMMON_RETURNS),
+    ("output_files", _COMMON_RETURNS) ]
+    
+
+def test_sets__common_functions():
+    def build_func_mocks(func_name, arguments, return_value):
+        for return_value in _COMMON_RETURNS:
+            cmd_mock = flexmock(AtomicCmd(["ls"]))
+            cmd_mock.should_receive(func_name).with_args(*arguments).and_return([return_value]).once
+            yield cmd_mock
+
+    def test_function(cmdclass, func_name, arguments, return_value):
+        commandset = cmdclass(build_func_mocks(func_name, arguments, return_value))
+        cmd_prop   = getattr(commandset, func_name)(*arguments)              
+        assert cmd_prop == return_value
+
+    for cmdclass in (ParallelCmds, SequentialCmds):
+        for (func_name, arguments, return_value) in _COMMON_FUNCTIONS:
+            yield test_function, cmdclass, func_name, arguments, return_value
+
+
+def test_sets__common_properties():
+    def build_prop_mocks(prop_name, return_value):
+        for return_value in _COMMON_RETURNS:
+            properties = dict([(prop_name, [return_value])])
+            yield flexmock(AtomicCmd(["ls"]), **properties)
+
+    def test_property(cmdclass, prop_name, return_value):
+        commandset = cmdclass(build_prop_mocks(prop_name, return_value))
+        cmd_prop   = getattr(commandset, prop_name)
+        assert cmd_prop == return_value
+        
+    for cmdclass in (ParallelCmds, SequentialCmds):
+        for (prop_name, return_values) in _COMMON_PROPERTIES:
+            yield test_property, cmdclass, prop_name, return_values
+
+
+def test_sets__str__():
+    def test_function(cls):
+        mocks = []
+        for ii in range(3):
+            cmd_mock = flexmock(AtomicCmd(["ls"]))
+            cmd_mock.should_receive("__str__").with_args().and_return(str(10 ** ii))
+            mocks.append(cmd_mock)
+        commands = cls(mocks)
+        description = str(commands)
+
+        assert description == "[1, 10, 100]"
+
+    yield test_function, ParallelCmds
+    yield test_function, SequentialCmds
+
+
+
+################################################################################
+################################################################################
+## Parallel commands
+
+def test_parallel_commands__atomiccmds():
+    mocks = []
+    for _ in range(3):
+        cmd_mock = flexmock(AtomicCmd(["ls"]))
+        cmd_mock.should_receive('run').with_args("xTMPx").once
+        mocks.append(cmd_mock)
+
+    cmds = ParallelCmds(mocks)
+    cmds.run("xTMPx")
+
+@nose.tools.raises(CmdError)
+def test_parallel_commands__reject_sequential():
+    command = AtomicCmd(["ls"])
+    seqcmd  = SequentialCmds([command])
+    ParallelCmds([seqcmd])
+
+def test_parallel_commands__accept_parallel():
+    command = AtomicCmd(["ls"])
+    parcmd  = ParallelCmds([command])
+    ParallelCmds([parcmd])
+
+@nose.tools.raises(CmdError)
+def test_parallel_commands__reject_noncommand():
+    ParallelCmds([object()])
+
+@nose.tools.raises(CmdError)
+def test_parallel_commands__reject_empty_commandset():
+    ParallelCmds([])
+
+
+
+################################################################################
+################################################################################
+## Sequential commands
+
+def test_sequential_commands__atomiccmds():
+    mocks = []
+    for _ in range(3):
+        cmd_mock = flexmock(AtomicCmd(["ls"]))
+        cmd_mock.should_receive('run').with_args("xTMPx").ordered.once
+        cmd_mock.should_receive('join').with_args().and_return([0]).ordered.once
+        mocks.append(cmd_mock)
+    
+    cmds = SequentialCmds(mocks)
+    cmds.run("xTMPx")
+
+def test_sequential_commands__abort_on_error():
+    cmd_mock_1 = flexmock(AtomicCmd(["ls"]))
+    cmd_mock_1.should_receive('run').with_args("xTMPx").ordered
+    cmd_mock_1.should_receive('join').with_args().and_return([0])
+    cmd_mock_2 = flexmock(AtomicCmd(["ls"]))
+    cmd_mock_2.should_receive('run').with_args("xTMPx").ordered
+    cmd_mock_2.should_receive('join').with_args().and_return([1])
+    cmd_mock_3 = flexmock(AtomicCmd(["ls"]))
+    cmd_mock_3.should_receive('run').with_args("xTMPx").never
+    cmd_mock_3.should_receive('join').with_args().and_return([None])
+   
+    cmds = SequentialCmds([cmd_mock_1, cmd_mock_2, cmd_mock_3])
+    cmds.run("xTMPx")
+
+    assert cmds.join() == [0, 1, None]
+
+
+def test_sequential_commands__accept_parallel():
+    command = AtomicCmd(["ls"])
+    parcmd  = ParallelCmds([command])
+    SequentialCmds([parcmd])
+
+def test_sequential_commands__accept_sequential():
+    command = AtomicCmd(["ls"])
+    seqcmd  = SequentialCmds([command])
+    SequentialCmds([seqcmd])
+
+@nose.tools.raises(CmdError)
+def test_parallel_commands__reject_noncommand():
+    SequentialCmds([object()])
+
+
+@nose.tools.raises(CmdError)
+def test_sequential_commands__reject_empty_commandset():
+    SequentialCmds([])
+    
---
+++ tests/node_test.py
@@ -0,0 +1,376 @@
+import os
+
+import nose.tools
+from flexmock import flexmock
+
+import fileutils
+import node as node_module
+from node import Node, MetaNode, CommandNode, NodeError, NodeUnhandledException
+
+# Monkey-patch create_tmp_dir, to avoid creating a lot of temp folders
+def create_temp_dir(root):
+    assert root == "/tmp"
+    return "/tmp/xTMPx"
+node_module.create_temp_dir = create_temp_dir
+
+def rmdir(path):
+    assert path == "/tmp/xTMPx"
+node_module.rmdir = rmdir
+
+
+
+_DESCRIPTION = "My description of a node"
+_IN_FILES    = ("tests/data/empty_file_1", "tests/data/empty_file_2")
+_OUT_FILES   = ("tests/data/missing_out_file_1", "tests/data/missing_out_file_2")
+
+
+################################################################################
+################################################################################
+## Node: Constructor tests
+
+def test_constructor__single_input_file():
+    my_node = Node(input_files  = _IN_FILES[0])
+    assert my_node.input_files == _IN_FILES[:1]
+
+def test_constructor__input_files():
+    my_node = Node(input_files  = _IN_FILES)
+    assert my_node.input_files == _IN_FILES
+
+
+
+def test_constructor__single_output_file():
+    my_node = Node(output_files  = _OUT_FILES[0])
+    assert my_node.output_files == _OUT_FILES[:1]
+
+def test_constructor__output_files():
+    my_node = Node(output_files  = _OUT_FILES)
+    assert my_node.output_files == _OUT_FILES
+
+
+
+def test_constructor__description():
+    my_node = Node(description  = _DESCRIPTION)
+    assert str(my_node) == _DESCRIPTION
+
+
+
+def test_constructor__single_subnode():
+    subnode = Node()
+    my_node  = Node(subnodes = subnode)
+    assert my_node.subnodes == set([subnode])
+
+def test_constructor__mult_subnodes():
+    subnodes = set([Node(), Node()])
+    my_node  = Node(subnodes = subnodes)
+    assert my_node.subnodes == subnodes
+
+@nose.tools.raises(TypeError)
+def test_constructor__subnode_not_a_node():
+    Node(subnodes = (1,))
+
+
+
+
+def test_constructor__single_dependencies():
+    dependencies = Node()
+    my_node  = Node(dependencies = dependencies)
+    assert my_node.dependencies == set([dependencies])
+
+def test_constructor__mult_dependencies():
+    dependencies = set([Node(), Node()])
+    my_node  = Node(dependencies = dependencies)
+    assert my_node.dependencies == dependencies
+
+@nose.tools.raises(TypeError)
+def test_constructor__dependency_not_a_node():
+    Node(dependencies = (1,))
+
+
+
+
+################################################################################
+################################################################################
+## Node: Properties tests
+
+def test_is_done__no_output():
+    assert Node().is_done
+
+def test_is_done__output_missing():
+    my_node = Node(output_files = "tests/data/missing_file")
+    assert not my_node.is_done
+
+def test_is_done__output_exists():
+    my_node = Node(output_files = "tests/data/empty_file_1")
+    assert my_node.is_done
+
+def test_is_done__updates():
+    my_node = Node(output_files = "tests/data/missing_file")
+    assert not my_node.is_done
+    my_node.output_files = ("tests/data/empty_file_1",)
+    assert my_node.is_done
+
+def test_is_done__subnodes_not_done():
+    my_subnode = Node(output_files = "tests/data/missing_file")
+    my_node    = Node(subnodes = my_subnode)
+    assert not my_node.is_done
+
+def test_is_done__subnodes_is_done():
+    my_subnode = Node(output_files = "tests/data/empty_file_1")
+    my_node    = Node(subnodes = my_subnode)
+    assert my_node.is_done
+
+def test_is_done__subnodes_updated():
+    my_subnode = Node(output_files = "tests/data/missing_file")
+    my_node    = Node(subnodes = my_subnode)
+    assert not my_node.is_done
+    my_subnode.output_files = ("tests/data/empty_file_1",)
+    assert my_node.is_done
+
+
+
+def test_is_outdated__no_output():
+    assert not Node().is_outdated
+
+def test_is_outdated__input_but_no_output():
+    assert not Node(input_files = _IN_FILES).is_outdated
+
+def test_is_outdated__output_but_no_input():
+    assert not Node(output_files = _OUT_FILES).is_outdated
+
+def test_is_outdated__not_outdated():
+    my_node = Node(input_files  = "tests/data/timestamp_younger",
+                   output_files = "tests/data/timestamp_older")
+    assert not my_node.is_outdated
+
+def test_is_outdated__outdated():
+    my_node = Node(input_files  = "tests/data/timestamp_older",
+                   output_files = "tests/data/timestamp_younger")
+    assert my_node.is_outdated
+
+def test_is_outdated__updates():
+    my_node = Node(input_files  = "tests/data/timestamp_younger",
+                   output_files = "tests/data/timestamp_older")
+    assert not my_node.is_outdated
+    my_node = Node(input_files  = "tests/data/timestamp_older",
+                   output_files = "tests/data/timestamp_younger")
+    assert my_node.is_outdated
+
+
+
+
+def test_run__order():
+    cfg_mock  = flexmock(temp_root = "/tmp")
+    node_mock = flexmock(Node())
+    node_mock.should_receive("_setup").with_args(cfg_mock, "/tmp/xTMPx").ordered.once
+    node_mock.should_receive("_run").with_args(cfg_mock, "/tmp/xTMPx").ordered.once
+    node_mock.should_receive("_teardown").with_args(cfg_mock, "/tmp/xTMPx").ordered.once
+    node_mock.run(cfg_mock)
+
+def test_run__temp_dirs():
+    paths = []
+    def assert_dir(_, path):
+        paths.append(path)
+
+    cfg_mock  = flexmock(temp_root = "/tmp")
+    node_mock = flexmock(Node(),
+                         _setup    = assert_dir,
+                         _run      = assert_dir,
+                         _teardown = assert_dir)
+    node_mock.run(cfg_mock)
+    assert len(set(paths)) == 1, paths
+
+
+def test_run__exceptions():
+    cfg_mock = flexmock(temp_root = "/tmp")
+    
+    def build_tests(key, exception, expectation):
+        @nose.tools.raises(expectation)
+        def test_function():
+            node_mock = flexmock(Node())
+            node_mock.should_receive(key).and_raise(exception).once
+            node_mock.run(cfg_mock)
+
+        return test_function
+
+    for key in ('_setup', '_run', '_teardown'):
+        yield build_tests(key, TypeError("The castle AAARGH!"), NodeUnhandledException)
+        yield build_tests(key, NodeError("He's a very naughty boy!"), NodeError)
+
+
+def test__setup__input_files_exist():
+    Node(input_files  = _IN_FILES)._setup(None, None)
+
+@nose.tools.raises(NodeError)
+def test__setup__input_files_missing():
+    Node(input_files  = _OUT_FILES)._setup(None, None)
+
+
+def test__setup__output_files_exist():
+    Node(output_files  = _IN_FILES)._teardown(None, None)
+
+@nose.tools.raises(NodeError)
+def test__setup__output_files_missing():
+    Node(output_files  = _OUT_FILES)._teardown(None, None)
+
+
+
+
+################################################################################
+################################################################################
+## CommandNode: Constructor 
+
+_SIMPLE_DEPS = Node()
+_SIMPLE_SUBS = Node()
+_SIMPLE_CMD_MOCK = flexmock(input_files  = _IN_FILES,
+                            output_files = _OUT_FILES)
+_SIMPLE_CMD_NODE = CommandNode(command      = _SIMPLE_CMD_MOCK,
+                               description  = "SimpleCommand",
+                               subnodes     = _SIMPLE_SUBS,
+                               dependencies = _SIMPLE_DEPS)
+
+def test_commandnode_constructor__description():
+    assert str(_SIMPLE_CMD_NODE) == "SimpleCommand"
+
+def test_commandnode_constructor__input_files():
+    assert _SIMPLE_CMD_NODE.input_files == _IN_FILES
+
+def test_commandnode_constructor__output_files():
+    assert _SIMPLE_CMD_NODE.output_files == _OUT_FILES
+                               
+def test_commandnode_constructor__subnodes():
+    assert _SIMPLE_CMD_NODE.subnodes == set([_SIMPLE_SUBS])
+
+def test_commandnode_constructor__dependencies():
+    assert _SIMPLE_CMD_NODE.dependencies == set([_SIMPLE_DEPS])
+
+
+################################################################################
+################################################################################
+## CommandNode: run
+
+def test_command_node__run():
+    cfg_mock  = flexmock(temp_root = "/tmp")
+    cmd_mock  = flexmock(input_files  = (),
+                         output_files = (),
+                         executables  = ())
+    node_mock = flexmock(CommandNode(cmd_mock))
+    node_mock.should_receive("_setup").with_args(cfg_mock, str).ordered.once
+    node_mock.should_receive("_run").with_args(cfg_mock, str).ordered.once
+    node_mock.should_receive("_teardown").with_args(cfg_mock, str).ordered.once
+    node_mock.run(cfg_mock)
+
+
+
+################################################################################
+################################################################################
+## CommandNode: _setup()
+
+def test_commandnode_setup__executable_missing():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = (),
+                        executables  = ("ls", "sh"))
+    node = CommandNode(cmd_mock)
+    node._setup(None, None)
+
+@nose.tools.raises(NodeError)
+def test_commandnode_setup__executables_exist():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = (),
+                        executables  = ("ls", "shxxxxxxxxxxxxx"))
+    node = CommandNode(cmd_mock)
+    node._setup(None, None)
+
+def test_commandnode_setup__input_exist():
+    cmd_mock = flexmock(input_files  = _IN_FILES,
+                        output_files = (),
+                        executables  = ())
+    node = CommandNode(cmd_mock)
+    node._setup(None, None)
+
+@nose.tools.raises(NodeError)
+def test_commandnode_setup__input_missing():
+    cmd_mock = flexmock(input_files  = _OUT_FILES,
+                        output_files = (),
+                        executables  = ())
+    node = CommandNode(cmd_mock)
+    node._setup(None, None)
+
+
+
+
+################################################################################
+################################################################################
+## CommandNode: _run()
+
+def test_commandnode_run__call_order():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = (),
+                        executables  = ())
+    cmd_mock.should_receive("run").with_args("xTMPx").ordered.once
+    cmd_mock.should_receive("join").with_args().and_return((0,)).ordered.once
+    node = CommandNode(cmd_mock)
+    node._run(None, "xTMPx")
+
+@nose.tools.raises(NodeError)
+def test_commandnode_run__exception_on_error():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = (),
+                        executables  = ())
+    cmd_mock.should_receive("run").ordered.once
+    cmd_mock.should_receive("join").and_return((1,)).ordered.once
+    node = CommandNode(cmd_mock)
+    node._run(None, None)
+
+
+
+################################################################################
+################################################################################
+## CommandNode: _teardown
+
+def test_commandnode_teardown__output_exists():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = _IN_FILES,
+                        executables  = ())
+    cmd_mock.should_receive("commit").once
+    node = CommandNode(cmd_mock)
+    node._teardown(None, None)
+    
+@nose.tools.raises(NodeError)
+def test_commandnode_teardown__output_missing():
+    cmd_mock = flexmock(input_files  = (),
+                        output_files = _OUT_FILES,
+                        executables  = ())
+    cmd_mock.should_receive("commit").once
+    node = CommandNode(cmd_mock)
+    node._teardown(None, None)
+    
+
+
+
+
+
+################################################################################
+################################################################################
+## MetaNode
+
+def test_metanode__run():
+    cfg_mock  = flexmock(temp_root = "/tmp")
+    node_mock = flexmock(MetaNode())
+    node_mock.should_receive('_setup').never
+    node_mock.should_receive('_run').never
+    node_mock.should_receive('_teardown').never
+
+    node_mock.run(cfg_mock)
+
+def test_metanode_is_done__no_subnodes():
+    assert MetaNode().is_done
+
+def test_metanode_is_done__subnodes_is_not_done():
+    my_subnode = Node(output_files = "tests/data/missing_file")
+    my_node    = MetaNode(subnodes = my_subnode)
+    assert not my_node.is_done
+
+def test_metanode_is_done__subnodes_is_done():
+    my_subnode = Node(output_files = "tests/data/empty_file_1")
+    my_node    = MetaNode(subnodes = my_subnode)
+    assert my_node.is_done
