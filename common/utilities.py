29f585bb9b392c7a1cd871e9c46ebd1ddc2ef85b
Mikkel Schubert <MikkelSch@gmail.com>
2012-07-30 15:09:12 +0200

    Cleanup using pylint.

--- common/utilities.py
+++ common/utilities.py
@@ -18,35 +18,35 @@ def safe_coerce_to_tuple(value):
         return (value,)
 
 
-def crc32(s):
-    return binascii.crc32(s) & 0xffffffff
+def crc32(data):
+    return binascii.crc32(data) & 0xffffffff
 
 
-def set_in(d, kws, value):
-    """Traverses a set of nested dictionaries using the keys in
-       kws, and assigns the specified value to the inner-most 
+def set_in(dictionary, keys, value):
+    """Traverses a set of nested dictionaries using the given keys, 
+       and assigns the specified value to the inner-most 
        dictionary (obtained from the second-to-last key), using
-       the last key in kws. Thus calling set_in is(d, [X, Y, Z], v)
+       the last key in keys. Thus calling set_in is(d, [X, Y, Z], v)
        is equivalent to calling 
          d.setdefault(X, {}).setdefault(Y, {})[Z] = v
 
        Behavior on non-dictionaries is undefined."""
-    kws = list(kws)
-    if not kws:
+    keys = list(keys)
+    if not keys:
         raise ValueError("No keys passed to 'set_in'!")
 
-    for kw in kws[:-1]:
+    for key in keys[:-1]:
         try:
-            d = d[kw]
+            dictionary = dictionary[key]
         except KeyError:
-            nd = {}
-            d[kw] = nd
-            d = nd
+            new_dict = {}
+            dictionary[key] = new_dict
+            dictionary = new_dict
 
-    d[kws[-1]] = value
+    dictionary[keys[-1]] = value
 
 
-def get_in(d, kws, default = None):
+def get_in(dictionary, keys, default = None):
     """Traverses a set of nested dictionaries using the keys in
        kws, and returns the value assigned to the final keyword
        in the innermost dictionary. Calling get_in(d, [X, Y])
@@ -55,26 +55,26 @@ def get_in(d, kws, default = None):
        to be returned.
 
        Behavior on non-dictgionaries is undefined."""
-    kws = list(kws)
-    for kw in kws[:-1]:
+    keys = list(keys)
+    for key in keys[:-1]:
         try:
-            d = d[kw]
+            dictionary = dictionary[key]
         except KeyError:
             return default
 
-    return d.get(kws[-1], default)
+    return dictionary.get(keys[-1], default)
 
 
-def split_before(it, key):
-    """Takes a sequence and splits it before every value where key(v) is true.
+def split_before(iterable, pred):
+    """Takes a sequence and splits it before every value where pred(v) is true.
     Thus split_before(range(10), key = lambda x: x % 2 == 0) would return the
     sequence [[1], [2,3], [4,5], [6,7], [7,8], [9]]"""
     items = []
-    for v in it:
-        if key(v) and items:
+    for value in iterable:
+        if pred(value) and items:
             yield items
             items = []
-        items.append(v)
+        items.append(value)
 
     if items:
         yield items
@@ -86,18 +86,18 @@ def is_strictly_increasing(lst):
 
 
 # Copied from the Python 'itertools' module documentation
-def grouper(n, iterable, fillvalue=None):
+def grouper(size, iterable, fillvalue=None):
     "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
+    args = [iter(iterable)] * size
     return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 
-def group_by_pred(pred, it):
+def group_by_pred(pred, iterable):
     """Splits items in a sequence into two lists, one containing
     items matching the predicate, and another containing those that
     do not."""
     is_true, is_false = [], []
-    for item in it:
+    for item in iterable:
         if pred(item):
             is_true.append(item)
         else:
