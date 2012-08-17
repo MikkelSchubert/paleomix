55ae99556ee40cf388ac1dbe06bf98ae4fe2ec56
Mikkel Schubert <MikkelSch@gmail.com>
2012-08-17 16:34:41 +0200

    Add documentation to generated mkfiles.

--- tools/alignment_mkfile.py
+++ tools/alignment_mkfile.py
@@ -22,11 +22,45 @@
 #
 import os
 import sys
+import datetime
 
+import pypeline.ui as ui
 from pypeline.common.text import padded_table
+
+
+_HEADER = \
+"""# Timestamp: %s
+# 
+# Columns:
+#     Name:     Name of the target. Final BAM filenames will consist of this
+#               name and the name of the BWA prefix joined by a dot. For
+#               example, given the name 'foobar' and the BWA prefix 
+#               'path/to/indices/hg19', the final BAM files will be named
+#               'foobar.hg19.bam' and 'foobar.hg19.unaligned.bam'.
+#     Sample:
+#     Library:  
+#     Barcode:  The Sample, Library, and Barcode (or other unique ID) of the
+#               lane. This is saved in the 'SM', 'LB' and 'PU'. 
+#     Platform: The sequencing platform used to generate this data. 
+#     Path:     Path to FASTQ files. This uses the Python 'glob' module to find
+#               files, and may therefore contain wildcards (*, [...]). For PE
+#               reads, replace the mate number with "{Pair}". 
+#               For example
+#                  /path/to/files/reads_R{Pair}_*.fastq.gz
+#
+# Data is organized by Name/Sample/Library/Barcode. Therefore, this combination
+# of columns MUST be unique. To prevent duplicate removal being done on multiple 
+# different samples, it is enforced that all libraries are assosiated with ONE 
+# sample only for any one target. Finally, any input file may only be specified
+# ONCE. The pipeline will not run if any of these assumptions are violated.
+#
+#"""
+
+
+_FILENAME = "SampleSheet.csv"
                      
 def read_alignment_records(root):
-    with open(os.path.join(root, "SampleSheet.csv")) as records:
+    with open(os.path.join(root, _FILENAME)) as records:
         header = records.readline().strip().split(",")
         for line in records:
             yield dict(zip(header, line.strip().split(",")))
@@ -47,8 +81,16 @@ def main(argv):
     lines.sort(key = lambda v: map(str.lower, v))
     lines.insert(0, ["Name", "Sample", "Library", "Barcode", "Platform", "Path"])
 
+    print _HEADER % datetime.datetime.now().isoformat()
     for line in padded_table(lines):
         print line
+
+    if not argv:
+        ui.print_info("No directories specified, empty table printed:", file = sys.stderr)
+        ui.print_info("\tUsage: %s [directory ...]" % sys.argv[0], file = sys.stderr)
+        ui.print_info("Each directory must contain a '%s' file." % _FILENAME, file = sys.stderr)
+    else:
+        ui.print_info("Makefile printed. Please check for correctness and update Path column before running pipeline.", file = sys.stderr)
 
 
 if __name__ == "__main__":
