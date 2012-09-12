e5d1e02d9a5d96221d0dc559ab7e2c22fa0838bb
Mikkel Schubert <MikkelSch@gmail.com>
2012-09-12 15:03:38 +0200

    Adding nodes to filter singleton changes in MSAs.

--- nodes/sequences.py
+++ nodes/sequences.py
@@ -27,9 +27,11 @@ import textwrap
 
 import pysam
 import pypeline.common.fileutils as fileutils
+import pypeline.common.formats.msa as msa
 
-from pypeline.node import Node
+from pypeline.node import Node, MetaNode
 from pypeline.common.formats.fasta import read_fasta
+from pypeline.common.utilities import safe_coerce_to_tuple
 
 
 
@@ -74,3 +76,57 @@ class CollectSequencesNode(Node):
             outfile  = os.path.join(self._destination, filename)
         
             fileutils.move_file(infile, outfile)
+
+
+class FilterSingletonsNode(Node):
+    def __init__(self, input_file, output_file, filter_by, dependencies):
+        self._input_file      = input_file
+        self._output_file     = output_file
+        self._filter_by       = dict(filter_by)
+
+        Node.__init__(self,
+                      description  = "<FilterSingleton: '%s' -> '%s'>" \
+                            % (input_file, output_file),
+                      input_files  = [input_file],
+                      output_files = [output_file],
+                      dependencies = dependencies)
+
+    def _run(self, _config, temp):
+        alignment = msa.read_msa(self._input_file)
+
+        for to_filter in self._filter_by:
+            groups = set(self._filter_by[to_filter])
+            groups.add(to_filter)
+
+            sequences = [alignment[group] for group in groups]
+            sequence = list(alignment[to_filter])
+            for (index, nts) in enumerate(zip(*sequences)):
+                nt = sequence[index]
+                if (nt not in "Nn-") and (nts.count(nt) == 1):
+                    sequence[index] = 'n'
+
+            alignment[to_filter] = "".join(sequence)
+
+        temp_filename = fileutils.reroot_path(temp, self._output_file)
+        msa.write_msa(alignment, temp_filename)
+        fileutils.move_file(temp_filename, self._output_file)
+       
+
+
+
+class FilterSingletonsMetaNode(MetaNode):
+    def __init__(self, input_files, destination, filter_by, dependencies = ()):
+        subnodes = []
+        filter_by = dict(filter_by)
+        for (filename, node) in input_files.iteritems():
+            output_filename = fileutils.reroot_path(destination, filename)
+            subnodes.append(FilterSingletonsNode(input_file   = filename,
+                                                 output_file  = output_filename,
+                                                 filter_by    = filter_by,
+                                                 dependencies = node))
+
+        MetaNode.__init__(self,
+                          description  = "<FilterSingleton: %i files -> '%s'>" \
+                            % (len(subnodes), destination),
+                          subnodes     = subnodes,
+                          dependencies = dependencies)
