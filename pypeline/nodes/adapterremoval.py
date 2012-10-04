c5df7559c1d0814399d8b015b5eccf67bcded791
Mikkel Schubert <MikkelSch@gmail.com>
2012-10-04 15:05:26 +0200

    Increase verbosity of description.

--- pypeline/nodes/adapterremoval.py
+++ pypeline/nodes/adapterremoval.py
@@ -74,8 +74,9 @@ class SE_AdapterRemovalNode(CommandNode):
         commands = ParallelCmds([adapterrm, gzip_discarded, gzip_truncated, zcat])
         CommandNode.__init__(self,
                              command      = commands,
-                             description  = "<SE_AdapterRM: %s>" \
-                                 % (self._desc_files(parameters.input_files),),
+                             description  = "<SE_AdapterRM: %s -> '%s.*'>" \
+                                 % (self._desc_files(parameters.input_files),
+                                    parameters.output_prefix),
                              dependencies = parameters.dependencies)
 
 
@@ -152,8 +153,9 @@ class PE_AdapterRemovalNode(CommandNode):
                                  zcat_pair_1,
                                  zcat_pair_2])
 
-        description  = "<PE_AdapterRM: %s>" \
-            % (self._desc_files(parameters.input_files_1).replace("file", "pair"),)
+        description  = "<PE_AdapterRM: %s -> '%s.*'>" \
+            % (self._desc_files(parameters.input_files_1).replace("file", "pair"),
+               parameters.output_prefix)
 
         CommandNode.__init__(self,
                              command      = commands,
