38aa4a9368e1c19ffea93a2328d523af8d747c4d
Mikkel Schubert <MikkelSch@gmail.com>
2013-03-19 18:10:47 +0400

    Make AdapterRemoval v1.3-GG1 the required version.

--- pypeline/nodes/adapterremoval.py
+++ pypeline/nodes/adapterremoval.py
@@ -27,6 +27,20 @@ from pypeline.atomiccmd import AtomicCmd, CmdError
 from pypeline.atomicset import ParallelCmds
 from pypeline.atomicparams import *
 
+import pypeline.common.versions as versions
+
+
+# To avoid problems caused by the many (unofficial) verisons of AdapterRemoval,
+# only a single know-to-work version is allowed at present. This version is
+# based on the official v1.3, with a patch applied (see pypeline/patches).
+def _adapterrm_patchlevel(value, pprint):
+    if value[-1] is None:
+        raise versions.VersionRequirementError("Please apply the AdapterRemoval patch in the pypeline/patches folder for version 1.3-GG1.")
+
+ADAPTERRM_VERSION = versions.Requirement(call   = ("AdapterRemoval", "--version"),
+                                         search = r"ver. (\d+)\.(\d+)(?:-GG(\d+))?",
+                                         pprint = "{}.{}-GG{}",
+                                         checks = versions.And(_adapterrm_patchlevel, versions.EQ(1, 3, 1)))
 
 
 class SE_AdapterRemovalNode(CommandNode):
@@ -99,8 +113,16 @@ class PE_AdapterRemovalNode(CommandNode):
         cmd.set_parameter("--file1",    "%(TEMP_IN_READS_1)s")
         cmd.set_parameter("--file2",    "%(TEMP_IN_READS_2)s")
 
-        # Prefix for output files
+        # Prefix for output files, ensure that all end up in temp folder
         cmd.set_parameter("--basename", "%(TEMP_OUT_BASENAME)s")
+        # Output files are explicity specified, to ensure that the order is the same here
+        # as below. A difference in the order in which files are opened can cause a deadlock,
+        # due to the use of named pipes (see __init__).
+        cmd.set_parameter("--output1", "%(TEMP_OUT_LINK_PAIR1)s")
+        cmd.set_parameter("--output2", "%(TEMP_OUT_LINK_PAIR2)s")
+        cmd.set_parameter("--outputcollapsed", "%(TEMP_OUT_LINK_ALN)s")
+        cmd.set_parameter("--singleton", "%(TEMP_OUT_LINK_UNALN)s")
+        cmd.set_parameter("--discarded", "%(TEMP_OUT_LINK_DISC)s")
 
         basename = os.path.basename(output_prefix)
         cmd.set_paths(# Only settings file is saved, rest is temporary files
@@ -112,11 +134,11 @@ class PE_AdapterRemovalNode(CommandNode):
                       TEMP_IN_READS_2     = "uncompressed_input_2",
 
                       # Named pipes for output of AdapterRemoval
-                      TEMP_OUT_LINK_1     = basename + ".singleton.aln.truncated",
-                      TEMP_OUT_LINK_2     = basename + ".singleton.unaln.truncated",
-                      TEMP_OUT_LINK_3     = basename + ".pair1.truncated",
-                      TEMP_OUT_LINK_4     = basename + ".pair2.truncated",
-                      TEMP_OUT_LINK_5     = basename + ".discarded",
+                      TEMP_OUT_LINK_ALN   = basename + ".collapsed",
+                      TEMP_OUT_LINK_UNALN = basename + ".singleton.truncated",
+                      TEMP_OUT_LINK_PAIR1 = basename + ".pair1.truncated",
+                      TEMP_OUT_LINK_PAIR2 = basename + ".pair2.truncated",
+                      TEMP_OUT_LINK_DISC  = basename + ".discarded",
                       TEMP_OUT_LINK_6     = "uncompressed_input_1",
                       TEMP_OUT_LINK_7     = "uncompressed_input_2")
 
@@ -135,19 +157,19 @@ class PE_AdapterRemovalNode(CommandNode):
         zcat_pair_2    = _build_unicat_command(parameters.input_files_2, "uncompressed_input_2")
         gzip_pair_1    = _build_gzip_command(parameters.output_prefix, ".pair1.truncated")
         gzip_pair_2    = _build_gzip_command(parameters.output_prefix, ".pair2.truncated")
-        gzip_aligned   = _build_gzip_command(parameters.output_prefix, ".singleton.aln.truncated")
-        gzip_unaligned = _build_gzip_command(parameters.output_prefix, ".singleton.unaln.truncated")
+        gzip_aligned   = _build_gzip_command(parameters.output_prefix, ".collapsed",           output = ".singleton.aln.truncated")
+        gzip_unaligned = _build_gzip_command(parameters.output_prefix, ".singleton.truncated", output = ".singleton.unaln.truncated")
         gzip_discarded = _build_gzip_command(parameters.output_prefix, ".discarded")
         adapterrm      = parameters.command.finalize()
 
         # Opening of pipes block, so the order of these commands is dependent upon
         # the order of file-opens in atomiccmd and the the programs themselves.
         commands = ParallelCmds([adapterrm,
-                                 gzip_discarded,
                                  gzip_pair_1,
                                  gzip_pair_2,
                                  gzip_aligned,
                                  gzip_unaligned,
+                                 gzip_discarded,
                                  zcat_pair_1,
                                  zcat_pair_2])
 
@@ -165,8 +187,8 @@ class PE_AdapterRemovalNode(CommandNode):
         os.mkfifo(os.path.join(temp, self._basename + ".discarded"))
         os.mkfifo(os.path.join(temp, self._basename + ".pair1.truncated"))
         os.mkfifo(os.path.join(temp, self._basename + ".pair2.truncated"))
-        os.mkfifo(os.path.join(temp, self._basename + ".singleton.aln.truncated"))
-        os.mkfifo(os.path.join(temp, self._basename + ".singleton.unaln.truncated"))
+        os.mkfifo(os.path.join(temp, self._basename + ".collapsed"))
+        os.mkfifo(os.path.join(temp, self._basename + ".singleton.truncated"))
         os.mkfifo(os.path.join(temp, "uncompressed_input_1"))
         os.mkfifo(os.path.join(temp, "uncompressed_input_2"))
 
@@ -186,20 +208,19 @@ def _build_unicat_command(input_files, output_file):
     return AtomicCmd(call, **paths)
 
 
-def _build_gzip_command(prefix, name):
+def _build_gzip_command(prefix, name, output = None):
     basename = os.path.basename(prefix)
     return AtomicCmd(["gzip", "-c", "-n"],
                      TEMP_IN_STDIN = basename + name,
-                     OUT_STDOUT    = prefix + name + ".gz")
+                     OUT_STDOUT    = prefix + (output or name) + ".gz")
 
 
 def _get_common_parameters():
-    cmd = AtomicParams("AdapterRemoval")
+    cmd = AtomicParams("AdapterRemoval",
+                       CHECK_VERSION = ADAPTERRM_VERSION)
 
     # Allow 1/3 mismatches in the aligned region
     cmd.set_parameter("--mm", 3, fixed = False)
-    # Reverse complement of adapter (required for SE?)
-    cmd.set_parameter("--pcr2", "CAAGCAGAAGACGGCATACGAGATNNNNNNGTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT", fixed = False)
     # Minimum length of trimmed reads
     cmd.set_parameter("--minlength", 25, fixed = False)
     # Trim Ns at read ends
