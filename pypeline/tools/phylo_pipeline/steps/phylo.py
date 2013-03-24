a2b69ec03064408ed3aff521ce8afa8b5858238d
Mikkel Schubert <MikkelSch@gmail.com>
2013-03-24 13:03:44 +0400

    Initial EXaML support.

--- pypeline/tools/phylo_pipeline/steps/phylo.py
+++ pypeline/tools/phylo_pipeline/steps/phylo.py
@@ -26,6 +26,7 @@ import os
 import sys
 
 from pypeline import Pypeline
+from pypeline.node import MetaNode
 from pypeline.nodes.formats import FastaToPartitionedInterleavedPhyNode
 from pypeline.nodes.raxml import *
 from pypeline.nodes.mafft import MetaMAFFTNode
@@ -61,37 +62,67 @@ def build_supermatrix(options, settings, destination, intervals, taxa, filtering
                            dependencies     = supermatrix)
 
 
+def _examl_nodes(destination, input_alignment, input_binary, dependencies):
+    initial_tree = os.path.join(destination, "initial.tree")
+
+    tree = ParsimonatorNode(input_alignment = input_alignment,
+                            output_tree     = initial_tree,
+                            dependencies    = dependencies)
+
+    return EXaMLNode(input_binary    = input_binary,
+                     initial_tree    = initial_tree,
+                     output_template = os.path.join(destination, "RAxML_%s"),
+                     dependencies    = tree)
+
+
 def build_examl_nodes(options, settings, intervals, taxa, filtering, dependencies):
     filtering_postfix = ".filtered" if any(filtering.itervalues()) else ""
     destination = os.path.join(options.destination, "phylogenies", "examl.supermatrix" + filtering_postfix)
 
     input_alignment = os.path.join(destination, "alignments.reduced.phy")
+    input_partition = os.path.join(destination, "alignments.reduced.partitions")
     input_binary    = os.path.join(destination, "alignments.reduced.binary")
 
     supermatrix = build_supermatrix(options, settings, destination, intervals, taxa, filtering_postfix, dependencies)
     binary      = EXaMLParserNode(input_alignment = input_alignment,
-                                  input_partition = os.path.join(destination, "alignments.reduced.partitions"),
+                                  input_partition = input_partition,
                                   output_file     = input_binary,
                                   dependencies    = supermatrix)
 
     replicates = []
     for replicate_num in range(settings["ExaML"]["Replicates"]):
         replicate_destination = os.path.join(destination, "replicate_%04i" % replicate_num)
-        initial_tree = os.path.join(replicate_destination, "initial.tree")
+        replicates.append(_examl_nodes(replicate_destination, input_alignment, input_binary, binary))
 
-        tree = ParsimonatorNode(input_alignment = input_alignment,
-                                output_tree     = initial_tree,
-                                dependencies    = supermatrix)
+    bootstraps = []
+    for bootstrap_num in range(settings["ExaML"]["Bootstraps"]):
+        bootstrap_destination = os.path.join(destination, "bootstrap_%04i" % bootstrap_num)
+        bootstrap_alignment   = os.path.join(bootstrap_destination, "bootstrap.phy")
+        bootstrap_binary      = os.path.join(bootstrap_destination, "bootstrap.binary")
 
-        node = EXaMLNode(input_binary    = input_binary,
-                         initial_tree    = initial_tree,
-                         output_template = os.path.join(replicate_destination, "RAxML_%s"),
-                         dependencies    = tree)
+        bootstrap   = RAxMLBootstrapNode(input_alignment  = input_alignment,
+                                         input_partition  = input_partition,
+                                         output_alignment = bootstrap_alignment,
+                                         dependencies     = supermatrix)
 
-        replicates.append(node)
+        bs_binary   = EXaMLParserNode(input_alignment = bootstrap_alignment,
+                                      input_partition = input_partition,
+                                      output_file     = bootstrap_binary,
+                                      dependencies    = bootstrap)
+
+        bootstraps.append(_examl_nodes(bootstrap_destination, bootstrap_alignment, bootstrap_binary, bs_binary))
 
 
-    return replicates
+    return MetaNode(description = "EXaML",
+                    dependencies = (MetaNode(description  = "Bootstraps",
+                                             subnodes     = bootstraps,
+                                             dependencies = supermatrix),
+                                    MetaNode(description  = "Replicates",
+                                             subnodes     = replicates,
+                                             dependencies = binary)))
+
+
+    return bootstraps
 
 
 def chain_examl(pipeline, options, makefiles):
