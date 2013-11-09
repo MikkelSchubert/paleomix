0be44bb32d7c1dda489397de4d974b2ac3484131
Mikkel Schubert <MikkelSch@gmail.com>
2013-11-09 13:23:29 +0100

    Make min. coverage settable.

--- examples/nature_protocols/phylogeny/select_highly_covered_genes.py
+++ examples/nature_protocols/phylogeny/select_highly_covered_genes.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,37 +22,43 @@
 #
 import os
 import sys
+import optparse
 
 from pypeline.common.formats.msa import \
      MSA
 
-# Minimum fraction of bases that must be covered across all sequences
-_FRAC_BASES_COVERED = 0.8
-# Exclude the following sequence(s) when calculating covered bases
-_EXCLUDE_SAMPLES = frozenset(("Pi_nucl",))
-
-
-def _is_sufficently_covered(filepath):
+def _is_sufficently_covered(filepath, min_coverage):
     msa = MSA.from_file(filepath)
     if msa.seqlen() % 3:
         return False
 
-    filtered_msa = msa.exclude(_EXCLUDE_SAMPLES)
-
     total_bases_not_covered = 0
-    for fasta_record in filtered_msa:
+    for fasta_record in msa:
         total_bases_not_covered += fasta_record.sequence.upper().count("N")
         total_bases_not_covered += fasta_record.sequence.count("-")
 
-    frac_covered = (1.0 - total_bases_not_covered / float(len(filtered_msa) * filtered_msa.seqlen()))
-    return frac_covered >= _FRAC_BASES_COVERED
+    total_bases = float(len(msa) * msa.seqlen())
+    frac_covered = 1.0 - total_bases_not_covered / total_bases
+    return frac_covered >= min_coverage
 
 
 def main(argv):
-    for root_dir in argv:
+    usage = "%prog [options] <PATH>"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("--min-coverage", default=0.8, type=float,
+                      help="Minimum fraction of called bases in a MSA "
+                           "[%default]")
+
+    config, args = parser.parse_args(argv)
+    if not args:
+        parser.print_usage()
+        return 1
+
+    for root_dir in args:
         for filename in os.listdir(root_dir):
             if filename.endswith(".afa"):
-                if _is_sufficently_covered(os.path.join(root_dir, filename)):
+                fpath = os.path.join(root_dir, filename)
+                if _is_sufficently_covered(fpath, config.min_coverage):
                     sequence_name, _ = filename.rsplit(".", 1)
                     print(sequence_name)
 
