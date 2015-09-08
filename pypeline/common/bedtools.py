31ac99c632b5dbf5ace9bc4679e6abd1c0b5380e
Mikkel Schubert <MikkelSch@gmail.com>
2015-09-08 15:20:08 +0200

    Add new BED-record class to replace pysam.asBed.

--- pypeline/common/bedtools.py
+++ pypeline/common/bedtools.py
@@ -22,8 +22,120 @@
 #
 import pysam
 
-import pypeline.common.text as text
 import pypeline.common.fileutils as fileutils
+import pypeline.common.text as text
+
+
+def _strand_type(value):
+    if value not in ("+", "-"):
+        raise ValueError("Strand must be '+' or '-', not %r" % (value,))
+    return value
+
+
+_BED_DEFAULTS = ("", 0, 0, "", 0, "+")
+_BED_KEYS = ("contig", "start", "end", "name", "score", "strand")
+_BED_TYPES = (str, int, int, str, int, _strand_type)
+
+
+class BEDRecord(object):
+    """Class for parsing and representing a BED records.
+
+    The class has the following properties:
+       .name -> str
+       .start -> int (0-based)
+       .end -> int (1-based)
+       .name -> str
+       .score -> int
+       .strand -> '+' or '-'
+
+    These fields can also be accessed using the square brackets notation, which
+    also gives access to any additional values, after the strand column. Fields
+    default to 0 or empty string, except the strand which defaults to '+'.
+    """
+
+    def __init__(self, line=None, _len=None):
+        """Constructs a BED record from a line of text. The length of the
+        object matches the number of columns in the input line; in the case
+        incompatible values, the ValueError exception is raised.
+
+        The len parameter is unused, and included only for compatibility with
+        pysam parser objects, such as 'asBED'. No minimum number of columns are
+        required, and it is possible to construct an empty bed record.
+        """
+        self._fields = []
+
+        if line:
+            line = line.rstrip("\r\n").split('\t')
+            for value, func in zip(line, _BED_TYPES):
+                self._fields.append(func(value))
+
+            if len(line) > len(self._fields):
+                self._fields.extend(line[len(self._fields):])
+
+    def __len__(self):
+        """Returns the number of fields in the record; 0 .. N."""
+        return len(self._fields)
+
+    def __str__(self):
+        """Returns a string suitable for writing to a .bed file."""
+        return "\t".join(str(value) for value in self._fields)
+
+    def __repr__(self):
+        """Returns a printable representation of the record."""
+        fields = []
+        for name, value in zip(_BED_KEYS, self._fields):
+            fields.append("%s=%r" % (name, value))
+
+        fields.extend(repr(value) for value in self._fields[len(_BED_KEYS):])
+
+        return "BEDRecord(%s)" % (", ".join(fields))
+
+    def __getitem__(self, index):
+        return self._fields[index]
+
+    def __setitem__(self, index, value):
+        if len(self._fields) <= index:
+            defaults = _BED_DEFAULTS[len(self._fields):index + 1]
+            self._fields.extend(defaults)
+            while len(self._fields) <= index:
+                self._fields.append("")
+
+        if index < len(_BED_TYPES):
+            if type(_BED_TYPES[index]) is type:
+                if not isinstance(value, _BED_TYPES[index]):
+                    raise ValueError("Expected %s for BED field %i, got %r"
+                                     % (_BED_TYPES[index].__name__,
+                                        index + 1, value))
+            else:
+                value = _BED_TYPES[index](value)
+
+        self._fields[index] = value
+
+    def __cmp__(self, other):
+        if not isinstance(other, BEDRecord):
+            return cmp(self.__class__, other.__class__)
+
+        return cmp(self._fields, other._fields)
+
+    @classmethod
+    def _set_properties(cls):
+        for index, name in enumerate(_BED_KEYS):
+            setattr(cls, name, cls._new_attr(index))
+
+    @classmethod
+    def _new_attr(cls, index):
+        """Returns an getter / setter property for the given value."""
+        def _get(self):
+            return self._fields[index]
+
+        def _set(self, value):
+            self[index] = value
+
+        return property(_get, _set)
+
+
+# Fill out properties for BEDRecord
+BEDRecord._set_properties()
 
 
 def read_bed_file(filename):
---
+++ tests/unit/common_tests/bedtools_tests.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2012 Mikkel Schubert <MSchubert@snm.ku.dk>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Ignore "invalid" function names
+# pylint: disable=C0103
+# No need for docstrings
+# pylint: disable=C0111
+import os
+import stat
+import errno
+
+import nose
+from nose.tools import \
+    assert_equal, \
+    assert_not_equal, \
+    assert_raises
+
+import pypeline
+from pypeline.common.testing import \
+    with_temp_folder, \
+    Monkeypatch, \
+    SetWorkingDirectory, \
+    set_file_contents, \
+    get_file_contents
+
+from pypeline.common.bedtools import \
+    BEDRecord
+
+
+###############################################################################
+## BEDRecord constructor
+
+def test_bedrecord__constructor__defaults():
+    record = BEDRecord()
+    assert_equal(len(record), 0)
+    assert_equal(str(record), "")
+    assert_equal(repr(record), "BEDRecord()")
+
+
+def test_bedrecord__constructor__empty_string():
+    record = BEDRecord("")
+    assert_equal(len(record), 0)
+    assert_equal(str(record), "")
+    assert_equal(repr(record), "BEDRecord()")
+
+
+def test_bedrecord__constructor__3_fields():
+    text = "my_contig\t12\t345"
+    record = BEDRecord(text)
+
+    assert_equal(len(record), 3)
+    assert_equal(str(record), text)
+    assert_equal(repr(record),
+                 "BEDRecord(contig='my_contig', start=12, end=345)")
+
+
+def test_bedrecord__constructor__6_fields():
+    text = "my_contig\t12\t345\tmy_name\t-3\t-"
+    record = BEDRecord(text)
+
+    assert_equal(len(record), 6)
+    assert_equal(str(record), text)
+    assert_equal(repr(record),
+                 "BEDRecord(contig='my_contig', start=12, "
+                 "end=345, name='my_name', score=-3, strand='-')")
+
+
+def test_bedrecord__constructor__extra_fields():
+    text = "my_contig\t12\t345\tmy_name\t-3\t-\tfoo\tbar"
+    record = BEDRecord(text)
+
+    assert_equal(len(record), 8)
+    assert_equal(str(record), text)
+    assert_equal(repr(record),
+                 "BEDRecord(contig='my_contig', start=12, "
+                 "end=345, name='my_name', score=-3, strand='-', "
+                 "'foo', 'bar')")
+
+
+###############################################################################
+## BEDRecord accessors
+
+
+def test_bedrecord__accessors__3_fields():
+    record = BEDRecord("my_contig\t12\t345")
+
+    assert_equal(record.contig, "my_contig")
+    assert_equal(record.start, 12)
+    assert_equal(record.end, 345)
+    assert_raises(IndexError, lambda: record.name)
+    assert_raises(IndexError, lambda: record.score)
+    assert_raises(IndexError, lambda: record.strand)
+
+
+def test_bedrecord__accessors__6_fields():
+    record = BEDRecord("my_contig\t12\t345\tmy_name\t-3\t-")
+
+    assert_equal(record.contig, "my_contig")
+    assert_equal(record.start, 12)
+    assert_equal(record.end, 345)
+    assert_equal(record.name, "my_name")
+    assert_equal(record.score, -3)
+    assert_equal(record.strand, "-")
+
+
+def test_bedrecord__accessors__extra_fields():
+    text = "my_contig\t12\t345\tmy_name\t-3\t-\tfoo\tbar"
+    record = BEDRecord(text)
+
+    assert_equal(record[6], "foo")
+    assert_equal(record[7], "bar")
+
+
+def test_bedrecord__accessors__6_fields__getitem():
+    record = BEDRecord("my_contig\t12\t345\tmy_name\t-3\t-")
+
+    assert_equal(record[0], "my_contig")
+    assert_equal(record[1], 12)
+    assert_equal(record[2], 345)
+    assert_equal(record[3], "my_name")
+    assert_equal(record[4], -3)
+    assert_equal(record[5], "-")
+
+
+def test_bedrecord__setters__3_fields():
+    record = BEDRecord("my_contig\t12\t345")
+
+    record.contig = "chrZ"
+    assert_equal(record.contig, "chrZ")
+
+    record.end += 20
+    assert_equal(record.end, 365)
+
+    assert_equal(str(record), "chrZ\t12\t365")
+    assert_equal(repr(record),
+                 "BEDRecord(contig='chrZ', start=12, end=365)")
+
+
+def test_bedrecord__setters__type_errors():
+    record = BEDRecord("my_contig\t12\t345\tname\t0\t+")
+
+#    assert_raises(ValueError, lambda: setattr(record, "contig", 17))
+    assert_raises(ValueError, lambda: setattr(record, "start", "foo"))
+    assert_raises(ValueError, lambda: setattr(record, "end", "foo"))
+    assert_raises(ValueError, lambda: setattr(record, "name", 17.3))
+    assert_raises(ValueError, lambda: setattr(record, "score", "foo"))
+    assert_raises(ValueError, lambda: setattr(record, "strand", "foo"))
+
+
+def test_bedrecord__setters__unset_fields__at_end():
+    record = BEDRecord("my_contig\t12\t345")
+
+    record.name = "my_region"
+    assert_equal(record.name, "my_region")
+
+    record.score = -13
+    assert_equal(record.score, -13)
+
+    record.strand = '-'
+    assert_equal(record.strand, '-')
+
+    assert_equal(str(record), "my_contig\t12\t345\tmy_region\t-13\t-")
+    assert_equal(repr(record),
+                 "BEDRecord(contig='my_contig', start=12, end=345, "
+                 "name='my_region', score=-13, strand='-')")
+
+
+def test_bedrecord__setters__unset_fields__after_end():
+    record = BEDRecord("")
+    record.strand = "-"
+    assert_equal(str(record), "\t0\t0\t\t0\t-")
+
+    record = BEDRecord("my_name")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t0\t0\t\t0\t-")
+
+    record = BEDRecord("my_name\t17")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t17\t0\t\t0\t-")
+
+    record = BEDRecord("my_name\t17\t258")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t17\t258\t\t0\t-")
+
+    record = BEDRecord("my_name\t17\t258\tregion")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t17\t258\tregion\t0\t-")
+
+    record = BEDRecord("my_name\t17\t258\tregion\t33")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t17\t258\tregion\t33\t-")
+
+    record = BEDRecord("my_name\t17\t258\tregion\t33\t+")
+    record.strand = "-"
+    assert_equal(str(record), "my_name\t17\t258\tregion\t33\t-")
+
+
+def test_bedrecord__cmp():
+    record_1_txt = "my_contig\t12\t345\tmy_name\t-3\t-\tfoo"
+    record_1 = BEDRecord(record_1_txt)
+    record_2 = BEDRecord("chrZ\t132\t4345\tchrZ_region\t0\t+\tbar")
+
+    for idx in xrange(len(record_2)):
+        record_tmp = BEDRecord(record_1_txt)
+        assert_equal(record_1, record_tmp)
+        record_tmp[idx] = record_2[idx]
+        assert_not_equal(record_1, record_tmp)
+        record_tmp[idx] = record_1[idx]
+        assert_equal(record_1, record_tmp)
