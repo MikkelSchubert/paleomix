a1baa8c27d90cfe89fa8aa1a294dbe884d4058d3
Mikkel Schubert <MikkelSch@gmail.com>
2016-03-19 00:52:31 +0100

    Additional test to verify expected behavior.

--- tests/atomiccmd_test/builder_test.py
+++ tests/atomiccmd_test/builder_test.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -673,6 +673,12 @@ def test_apply_options__single_option__boolean__pop_when_value_is_false():
     mock = flexmock()
     mock.should_receive('pop_option').with_args('-v')
     apply_options(mock, {"-v": False})
+
+
+def test_apply_options__single_option__boolean__pop_missing_throws():
+    mock = flexmock()
+    mock.should_receive('pop_option').with_args('-v').and_raise(KeyError('-v'))
+    assert_raises(KeyError, apply_options, mock, {"-v": False})
 
 
 def test_apply_options__multiple_option():
