f9b611a597f3d4df74c8ea615a6ba9ce00917434
Mikkel Schubert <MikkelSch@gmail.com>
2016-11-28 11:29:09 +0100

    Faster way to check for required R modules.

--- paleomix/resources/rscripts/common/requires.r
+++ paleomix/resources/rscripts/common/requires.r
@@ -4,8 +4,13 @@ if (length(args) != 1) {
     quit(status=1)
 }
 
+packages <- installed.packages()
+
 for (module in args) {
-    if (!require(module, character.only=TRUE)) {
+    if (!(module %in% packages)) {
+        cat(paste("R module '", module, "' is not installed!\n", sep=''))
+        cat(paste("Please run R and execute the following command:\n\n"))
+        cat(paste("> install.packages('", module, "')\n", sep=''))
         quit(status=1)
     }
 
