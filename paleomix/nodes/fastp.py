40547c95f586aa8f178d051790b7b8a862f465dd
Mikkel Schubert <MikkelSch@gmail.com>
2021-03-22 15:14:17 +0100

    simplify fastp node using `merge_options`

--- paleomix/nodes/fastp.py
+++ paleomix/nodes/fastp.py
@@ -9,9 +9,6 @@ from paleomix.common.fileutils import describe_paired_files, reroot_path
 from paleomix.node import CommandNode, NodeError
 
 
-_NOT_SUPPORTED = set(["--overlapped_out", "--stdin", "--stdout", "--interleaved_in"])
-
-
 class FastpNode(CommandNode):
     def __init__(
         self,
@@ -21,14 +18,12 @@ class FastpNode(CommandNode):
         out_fq_2,
         out_merged,
         out_unpaired,
+        out_failed,
         out_html,
         out_json,
         options={},
         dependencies=(),
     ):
-        for key in _NOT_SUPPORTED & set(dict(options)):
-            raise NotImplementedError("fastp option {} not supported".format(key))
-
         command = AtomicCmd2(
             ["fastp"],
         )
@@ -36,30 +31,37 @@ class FastpNode(CommandNode):
         # All unpaired reads are written to the same file
         out_unpaired = OutputFile(out_unpaired)
 
-        fastp_options = {
-            "--merge": None,
-            "--in1": InputFile(in_fq_1),
-            "--in2": InputFile(in_fq_2),
-            "--out1": OutputFile(out_fq_1),
-            "--out2": OutputFile(out_fq_2),
-            "--merged_out": OutputFile(out_merged),
-            "--unpaired1": out_unpaired,
-            "--unpaired2": out_unpaired,
-            "--html": OutputFile(out_html),
-            "--json": OutputFile(out_json),
-        }
-
-        for key in set(options) & set(fastp_options):
-            raise NodeError("cannot set override fastp option {}".format(key))
-
-        fastp_options.update(options)
-        command.append_options(fastp_options)
+        command.merge_options(
+            user_options=options,
+            fixed_options={
+                # FIXME: Make merging optional
+                "--merge": None,
+                "--in1": InputFile(in_fq_1),
+                "--in2": InputFile(in_fq_2),
+                "--out1": OutputFile(out_fq_1),
+                "--out2": OutputFile(out_fq_2),
+                "--failed_out": OutputFile(out_failed),
+                "--merged_out": OutputFile(out_merged),
+                # Using the same output path for both --unpaired is supported by fastp
+                "--unpaired1": out_unpaired,
+                "--unpaired2": out_unpaired,
+                "--html": OutputFile(out_html),
+                "--json": OutputFile(out_json),
+            },
+            blacklisted_options=[
+                "--overlapped_out",
+                "--stdin",
+                "--stdout",
+                "--interleaved_in",
+            ],
+        )
 
         CommandNode.__init__(
             self,
             command=command,
             description="pre-processing %s"
             % (describe_paired_files([in_fq_1], [in_fq_2]),),
-            threads=options.get("--thread", 2),
+            # FIXME: Number of threads seems hard to predict; about --thread +1/+2?
+            threads=options.get("--thread", 2) + 1,
             dependencies=dependencies,
         )
