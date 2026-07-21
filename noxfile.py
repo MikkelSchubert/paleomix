7659a90f42989f85d26fab3df7dd95db04af082c
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-07-21 16:12:04 +0200

    drop support for py3.9, add py3.14 (#140)

--- noxfile.py
+++ noxfile.py
@@ -90,7 +90,7 @@ def tests(session: nox.Session) -> None:
     )
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def full_tests(session: nox.Session) -> None:
     session.install(
         ".",
